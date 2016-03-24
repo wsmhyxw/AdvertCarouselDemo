bf073136d722d1b826f7f1c2e8afaa68a2f67d7e
django <djangolee@sina.com>
2016-03-24 14:10:55 +0800

    Initial Commit

---
+++ AdvertCarouselDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdvertCarouselDemo
+//
+//  Created by django on 16/3/24.
+//  Copyright © 2016年 django. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
